eb6b83847ae35e181fd5ff85b13d361250f21eb5
Kirito <leshap94@gmail.com>
2019-05-25 17:25:39 +0600

    Урок 2

--- jsscript.js
+++ jsscript.js
@@ -1,23 +1,4 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ""), 
-    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
-    q1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a1 = prompt("Во сколько обойдется?", ""),
-    q2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a2 = prompt("Во сколько обойдется?", ""),
-    appData = {
-        budget: money,
-        timeData: time,
-        expenses: {
-            q1: a1,
-            q2: a2
-        },
-        optional: {
 
-        },
-        income: [],
-        savings: false
-    };
-alert("Бюджет на 1 день составляет: " 
-        + ((appData.budget - appData.expenses.q1 - appData.expenses.q2)/30));
+
